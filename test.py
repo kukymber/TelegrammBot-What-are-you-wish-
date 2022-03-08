f42d331ef51ad4987db30200acc4b0bbf19a5e49
kukymber <ostap_to@mail.ru>
2022-03-08 22:32:55 +0800

    create buttons

--- test.py
+++ test.py
@@ -19,12 +19,15 @@ class FSWaitAnswer(StatesGroup):
 
 @dp.message_handler(commands=['start'], state=None)  # устанавливаем команды, после них вывод
 async def command_start(message: Message, state: FSMContext):
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ["То что нужно", "Это то что ты хочешь"]
+    keyboard.add(*buttons)
     await message.answer(text='Привет')  # вывод на команду start
-    await message.answer(text='О чем ты мечтаешь?')
+    await message.answer(text='О чем ты мечтаешь?', reply_markup=keyboard)
     await FSWaitAnswer.what_you_need.set()  # устанавливаем состояние
 
 
-async def echo(message: Message):
+async def get_link_picture(message: Message):
     key_for_picture = message.text  # ключ по которому будет производится поиск
     url = f'https://www.google.ru/search?q={key_for_picture}&newwindow=1&espv=2&source=lnms&tbm=isch&sa=X'
     response = requests.get(url)  # запрос по адресу
@@ -39,14 +42,11 @@ async def echo(message: Message):
 @dp.message_handler(state=FSWaitAnswer.what_you_need)
 async def search_picture(message: Message, state: FSMContext):
     async with state.proxy() as data:
-        data['what_you_need'] = message  # не точно(Записывает сообщение в what_you_need)
-    # await FSWaitAnswer.next()  # не точно ( то же самое что и через прокси)
-    waiting_for_input = data['what_you_need']  # !!!
-    await echo(waiting_for_input)
-    # await state.finish()
-    await FSWaitAnswer.next()  # если отключить, то всегда перехватывает сообщения
+        data['what_you_need'] = message  # Записывает сообщение в what_you_need
+    waiting_for_input = data['what_you_need']  # экземпляр объекта what_you_need
+    await get_link_picture(waiting_for_input)  # вызов функции передаем полученое сообщение
+    await FSWaitAnswer.next()  # выбираем следущее состояние после того, которое в хандлере
     await message.answer(text='Это то что ты хочешь?')
-    # await FSWaitAnswer.waiting_for_agree.set()  # добавить иф на обработку двух вариантов
 
 
 async def continuation_search(message: Message, state=FSMContext):
@@ -56,40 +56,29 @@ async def continuation_search(message: Message, state=FSMContext):
 
 async def again_search(this_input_for_search):
     input_some = this_input_for_search
-    await echo(input_some)
+    await get_link_picture(input_some)
 
 
-@dp.message_handler(state=FSWaitAnswer.waiting_for_agree)
+@dp.message_handler(state=FSWaitAnswer.waiting_for_agree)  # выполняется только после await FSWaitAnswer.next()
 async def test(message: Message, state: FSMContext):
     if message.text == 'То что нужно':
-        await continuation_search(message, state=FSMContext)
+        await continuation_search(message, state=FSMContext)  # вызываем функцию продолжения поиска(ютуб и абзац)
     else:
-        # async with state.proxy() as data:
-        #     this_input_for_search = data['what_you_need']
         await bot.send_message(chat_id=message.chat.id, text="попробую снова")
-        async with state.proxy() as data:
-            this_input_for_search = data['what_you_need']
-        await again_search(this_input_for_search)
+        async with state.proxy() as data:  # вытаскиваем переменную
+            this_input_for_search = data['what_you_need']  # создаем экземпляр переменной
+        await again_search(this_input_for_search)  # вызов функции, передаем экземпляр
 
-
-@dp.message_handler()
-async def cmd_start(message: Message):
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    button_1 = KeyboardButton(text='То что нужно')
-    keyboard.add(button_1)
-    button_2 = 'Это не то что я хочу'
-    keyboard.add(button_2)
-    # await message.answer(reply_markup=keyboard)
+# @dp.message_handler()
+# async def cmd_start():
+#     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+#     button_1 = KeyboardButton(text='То что нужно')
+#     keyboard.add(button_1)
+#     button_2 = 'Это не то что я хочу'
+#     keyboard.add(button_2)
+#     await cmd_start(reply_markup=keyboard)
 
 # @dp.message_handler()
 # async def ask_user_agree(message: Message):
 #     if message.text in ['yes']
 #     await bot.send_message(chat_id=message.from_user.id, text='Это то, чего ты хочешь?')
-
-
-# @dp.message_handler()
-# async def echo(message: Message):  # функция ответа на строку ввода
-#     text = f'Привет, ты написал {message.text}'  # message.text = то что ввели
-#     await message.answer(text=text)  # выводит переменную text
-#     await bot.send_message(chat_id=message.from_user.id,
-#                            text=message.text)  # отвечает, то что мы ввели (message.text) - то что пришло в телеге
